c5288077bc47660efd14c57bb26657280d64d51f
Allison Youngblood <allison.a.youngblood@gmail.com>
2022-06-25 11:18:21 -0400

    adding if statement so the calculation doesn't re-run everytime

--- NHI_to_attenuation_uncertainty.py
+++ NHI_to_attenuation_uncertainty.py
@@ -22,144 +22,79 @@ mask = (wave == 100) + (wave == 200) + (wave == 300)
 h1_col_mean_array = np.arange(17.,19.6,0.1)
 h1_col_sig_array = np.array([0.01,0.05,0.1,0.2,0.3,0.4,0.5])
 
-fractional_uncertainties_to_store = np.zeros((len(h1_col_mean_array),len(h1_col_sig_array),3))
+he_uncertainty = False
 
+
+run = False
 ####
 
-n = 5000
-
-h1_col_mean=18.5
-h1_col_sig=0.3
-
-h1_cols = np.random.normal(loc=h1_col_mean,scale=h1_col_sig,size=n)
+if run:
 
 
-rel_he1_val = 0.08
-rel_he1_sig = 0.02
-frac_he2_val = 0.6
-frac_he2_sig = 0.2
-he_uncertainty = False
+    #########
+
+    wave = np.arange(100,901,10)
+    mask = (wave == 100) + (wave == 200) + (wave == 300) + (wave == 400) + (wave == 500) + (wave == 600) + (wave == 700) + (wave == 800) + (wave == 900)
+
+    h1_col_mean_array = np.arange(17.4,19.2,0.2)
+    h1_col_sig_array = np.logspace(-2,-0.3,10)#np.array([0.01,0.05,0.1,0.2,0.3,0.4,0.5])
+
+    rel_he1_val = 0.08
+    rel_he1_sig = 0.02
+    frac_he2_val = 0.6
+    frac_he2_sig = 0.2
+    
+
+    fractional_uncertainties_to_store = np.zeros((len(h1_col_mean_array),len(h1_col_sig_array),int(mask.sum())))
+
+    n=5000
 
 
+    for i in range(len(h1_col_mean_array)):
+
+        print("i = " + str(i) + ' / ' + str(len(h1_col_mean_array)))
+
+        h1_col_mean = h1_col_mean_array[i]
+
+        #plt.figure()
+
+        for j in range(len(h1_col_sig_array)):
+
+            print("j = " + str(j) + ' / ' + str(len(h1_col_sig_array)))
+
+            h1_col_sig = h1_col_sig_array[j]
+
+            h1_cols = np.random.normal(loc=h1_col_mean,scale=h1_col_sig,size=n)
 
 
-atten_array = np.zeros((n,len(wave)))
+            atten_array = np.zeros((n,len(wave)))
+
+            for k in range(n):
+
+                if he_uncertainty:
+                    rel_he1 = np.random.normal(loc=rel_he1_val,scale=rel_he1_sig)
+                    frac_he2 = np.random.normal(loc=frac_he2_val,scale=frac_he2_sig)
+                else:
+                    rel_he1 = rel_he1_val
+                    frac_he2 = frac_he2_val
 
 
-fig=plt.figure(figsize=(7,9))
-ax=fig.add_subplot(211)
-ax1 = fig.add_subplot(212)
+                atten_array[k,:] = total_tau_profile_func_euv(wave,h1_cols[k],h1_vel,rel_he1=rel_he1,frac_he2=frac_he2)
+                #atten_array[k,:] = total_tau_profile_func_euv(wave,h1_col_mean,h1_vel,rel_he1=rel_he1,frac_he2=frac_he2)
 
-for i in range(n):
+                plt.plot(wave,atten_array[k,:],color='k',alpha=0.5,linewidth=0.7)
 
-    if he_uncertainty:
-            rel_he1 = np.random.normal(loc=rel_he1_val,scale=rel_he1_sig)
-            frac_he2 = np.random.normal(loc=frac_he2_val,scale=frac_he2_sig)
-    else:
-            rel_he1 = rel_he1_val
-            frac_he2 = frac_he2_val
+            low1sig, median, high1sig = np.percentile(atten_array,[15.4,50,84.1],axis=0)
+            avg_1sig = np.mean([high1sig-median, median-low1sig],axis=0)
+            frac_uncertainty = avg_1sig/median
 
-    atten_array[i,:] = total_tau_profile_func_euv(wave,h1_cols[i],h1_vel,rel_he1=rel_he1,frac_he2=frac_he2,which_line='all')
+            fractional_uncertainties_to_store[i,j,:] = frac_uncertainty[mask]
 
-    if i%10 == 0:
-        ax.plot(wave,atten_array[i,:], color='k',alpha=0.1,linewidth=0.5)
+else:
+    
+    with open("../fractional_uncertainties_100_600_Ang.pkl", "rb") as f:
 
-
-low1sig, median, high1sig = np.percentile(atten_array,[15.4,50,84.1],axis=0)
-
-avg_1sig = np.mean([high1sig-median, median-low1sig],axis=0)
-
-
-ax.plot(wave,median,color='deeppink',label='Median with 1-sigma confidence interval')
-ax.plot(wave,low1sig,color='deeppink',linestyle='--')
-ax.plot(wave,high1sig,color='deeppink',linestyle='--')
-
-
-#ax.fill_between(wave,low1sig,high1sig,color='deeppink',alpha=0.8)
-
-ax1.set_xlabel('Wavelength (\AA)',fontsize=18)
-ax.set_ylabel('Transmission Fraction',fontsize=18)
-ax.minorticks_on()
-ax.legend()
-
-ax.set_title('log N(HI) = ' + str(h1_col_mean) + ' $\pm$ ' + str(h1_col_sig),fontsize=18)
-
-ax1.plot(wave,avg_1sig/median)
-#print(avg_1sig[mask]/median[mask])
-ax1.set_ylabel('Fractional Uncertainty',fontsize=18)
-ax1.minorticks_on()
-ax1.set_yscale('log')
-fig.tight_layout()
-
-ax.set_xlim([50,900])
-ax1.set_xlim([50,900])
-
-ax1.set_ylim([0.01,10])
-
-ax1.grid(True)
-
-
-
-
-#########
-
-wave = np.arange(100,901,10)
-mask = (wave == 100) + (wave == 200) + (wave == 300) + (wave == 400) + (wave == 500) + (wave == 600) + (wave == 700) + (wave == 800) + (wave == 900)
-
-h1_col_mean_array = np.arange(17.4,19.2,0.2)
-h1_col_sig_array = np.logspace(-2,-0.3,10)#np.array([0.01,0.05,0.1,0.2,0.3,0.4,0.5])
-
-rel_he1_val = 0.08
-rel_he1_sig = 0.02
-frac_he2_val = 0.6
-frac_he2_sig = 0.2
-he_uncertainty = False
-
-fractional_uncertainties_to_store = np.zeros((len(h1_col_mean_array),len(h1_col_sig_array),int(mask.sum())))
-
-n=5000
-
-
-for i in range(len(h1_col_mean_array)):
-
-    print("i = " + str(i) + ' / ' + str(len(h1_col_mean_array)))
-
-    h1_col_mean = h1_col_mean_array[i]
-
-    #plt.figure()
-
-    for j in range(len(h1_col_sig_array)):
-
-        print("j = " + str(j) + ' / ' + str(len(h1_col_sig_array)))
-
-        h1_col_sig = h1_col_sig_array[j]
-
-        h1_cols = np.random.normal(loc=h1_col_mean,scale=h1_col_sig,size=n)
-
-
-        atten_array = np.zeros((n,len(wave)))
-
-        for k in range(n):
-
-            if he_uncertainty:
-                rel_he1 = np.random.normal(loc=rel_he1_val,scale=rel_he1_sig)
-                frac_he2 = np.random.normal(loc=frac_he2_val,scale=frac_he2_sig)
-            else:
-                rel_he1 = rel_he1_val
-                frac_he2 = frac_he2_val
-
-
-            atten_array[k,:] = total_tau_profile_func_euv(wave,h1_cols[k],h1_vel,rel_he1=rel_he1,frac_he2=frac_he2)
-            #atten_array[k,:] = total_tau_profile_func_euv(wave,h1_col_mean,h1_vel,rel_he1=rel_he1,frac_he2=frac_he2)
-
-            plt.plot(wave,atten_array[k,:],color='k',alpha=0.5,linewidth=0.7)
-
-        low1sig, median, high1sig = np.percentile(atten_array,[15.4,50,84.1],axis=0)
-        avg_1sig = np.mean([high1sig-median, median-low1sig],axis=0)
-        frac_uncertainty = avg_1sig/median
-
-        fractional_uncertainties_to_store[i,j,:] = frac_uncertainty[mask]
-
+        h1_col_mean_array,h1_col_sig_array,fractional_uncertainties_to_store = joblib.load(f)
 
 
 fig = plt.figure(figsize=(12,12))
@@ -196,13 +131,18 @@ ax7.contourf(xx,yy,fractional_uncertainties_to_store[:,:,6].transpose(),vmin=0.0
 ax8.contourf(xx,yy,fractional_uncertainties_to_store[:,:,7].transpose(),vmin=0.01, vmax=1,levels=levels)
 ax9.contourf(xx,yy,fractional_uncertainties_to_store[:,:,8].transpose(),vmin=0.01, vmax=1,levels=levels)
 
-if False:
-    ax1.contour(xx,yy,fractional_uncertainties_to_store[:,:,0].transpose(),levels=[0.35],color='k')
-    ax2.contour(xx,yy,fractional_uncertainties_to_store[:,:,1].transpose(),levels=[0.35],color='k')
-    ax3.contour(xx,yy,fractional_uncertainties_to_store[:,:,2].transpose(),levels=[0.35],color='k')
-    ax4.contour(xx,yy,fractional_uncertainties_to_store[:,:,3].transpose(),levels=[0.35],color='k')
-    ax5.contour(xx,yy,fractional_uncertainties_to_store[:,:,4].transpose(),levels=[0.35],color='k')
-    ax6.contour(xx,yy,fractional_uncertainties_to_store[:,:,5].transpose(),levels=[0.35],color='k')
+if True:
+    lw=0.5
+    ax1.contour(xx,yy,fractional_uncertainties_to_store[:,:,0].transpose(),levels=levels,colors='k',linewidths=lw)
+    ax2.contour(xx,yy,fractional_uncertainties_to_store[:,:,1].transpose(),levels=levels,colors='k',linewidths=lw)
+    ax3.contour(xx,yy,fractional_uncertainties_to_store[:,:,2].transpose(),levels=levels,colors='k',linewidths=lw)
+    ax4.contour(xx,yy,fractional_uncertainties_to_store[:,:,3].transpose(),levels=levels,colors='k',linewidths=lw)
+    ax5.contour(xx,yy,fractional_uncertainties_to_store[:,:,4].transpose(),levels=levels,colors='k',linewidths=lw)
+    ax6.contour(xx,yy,fractional_uncertainties_to_store[:,:,5].transpose(),levels=levels,colors='k',linewidths=lw)
+    ax7.contour(xx,yy,fractional_uncertainties_to_store[:,:,6].transpose(),levels=levels,colors='k',linewidths=lw)
+    ax8.contour(xx,yy,fractional_uncertainties_to_store[:,:,7].transpose(),levels=levels,colors='k',linewidths=lw)
+    ax9.contour(xx,yy,fractional_uncertainties_to_store[:,:,8].transpose(),levels=levels,colors='k',linewidths=lw)
+
 
 
 ax1.minorticks_on()
@@ -215,9 +155,9 @@ ax7.minorticks_on()
 ax8.minorticks_on()
 ax9.minorticks_on()
 
-ax7.set_xlabel('log$_{10}$ N(HI) (dex)',fontsize=18)
-ax8.set_xlabel('log$_{10}$ N(HI) (dex)',fontsize=18)
-ax9.set_xlabel('log$_{10}$ N(HI) (dex)',fontsize=18)
+ax7.set_xlabel('log$_{10}$[N(HI)/cm$^{-2}$]',fontsize=18)
+ax8.set_xlabel('log$_{10}$[N(HI)/cm$^{-2}$]',fontsize=18)
+ax9.set_xlabel('log$_{10}$[N(HI)/cm$^{-2}$]',fontsize=18)
 
 
 ax1.set_ylabel('$\sigma_{N(HI)}$ (dex)',fontsize=18)
@@ -259,8 +199,9 @@ else:
 
     filename = "../fractional_uncertainties_100_600_Ang.pkl"
 
-with open(filename, "wb") as f:
 
+if not run: 
+    with open(filename, "wb") as f:
         joblib.dump([h1_col_mean_array,h1_col_sig_array,fractional_uncertainties_to_store], f)
 
 plt.savefig(filename.replace('.pkl','.png'))
